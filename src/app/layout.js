655be8cf9f840a4f8c7d24177bdf88bdcd99eab5
hoyo0822 <wjdghdud0822@gmail.com>
2026-06-09 02:24:46 +0900

    Add Vercel SpeedInsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import { ThemeProvider } from "../components/ThemeProvider";
 import lab from "../../data/lab.json";
 import Image from "next/image";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -67,6 +68,7 @@ export default function RootLayout({ children }) {
 
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
