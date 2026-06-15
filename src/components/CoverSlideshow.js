04c417e6a29d82cf03611ec25b1386da3c0d1e8f
naongee <dgfgd7825@gmail.com>
2026-06-15 17:40:03 +0900

    Update CoverSlideshow.js

--- src/components/CoverSlideshow.js
+++ src/components/CoverSlideshow.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const AUTO_SLIDE_MS = 5000;
-const FADE_MS = 320;
+const FADE_MS = 500;
 
 export default function CoverSlideshow({ covers }) {
   const [current, setCurrent] = useState(0);
